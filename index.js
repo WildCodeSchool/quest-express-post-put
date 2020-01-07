804aaea71aa7571dd976d3408e3c1007789f0618
Benoît Hubert <benoithubert@gmail.com>
2020-01-07 13:41:12 +0100

    Validate inputs with express-validator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 const connection = require('./db');
 
 const app = express();
@@ -25,19 +26,30 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-app.post('/api/users', (req, res) => {
+app.post('/api/users', [
+  // email must be valid
+  check('email').isEmail(),
+  // password must be at least 8 chars long
+  check('password').isLength({ min: 8 }),
+  // let's assume a name should be 2 chars long
+  check('name').isLength({ min: 2 }),
+],
+(req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   // send an SQL query to get all users
-  connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
+  return connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
     if (err) {
       // If an error has occurred, then the client is informed of the error
-      res.status(500).json({
+      return res.status(500).json({
         error: err.message,
         sql: err.sql,
       });
-    } else {
-      // If everything went well, we send the result of the SQL query as JSON
-      res.json(results);
     }
+    // If everything went well, we send the result of the SQL query as JSON
+    return res.json(results);
   });
 });
 
