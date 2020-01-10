175d95a2acdfb60b7f6b84e633619d30d2d5763f
Benoît Hubert <benoithubert@gmail.com>
2020-01-10 10:33:25 +0100

    Return the new user as res. body, with 201 and Location header

--- index.js
+++ index.js
@@ -48,8 +48,28 @@ app.post('/api/users', [
         sql: err.sql,
       });
     }
-    // If everything went well, we send the result of the SQL query as JSON
-    return res.json(results);
+    // We use the insertId attribute of results to build the WHERE clause
+    return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
+      if (err2) {
+        return res.status(500).json({
+          error: err2.message,
+          sql: err2.sql,
+        });
+      }
+      // If all went well, records is an array, from which we use the 1st item
+      const insertedUser = records[0];
+      // Extract all the fields *but* password as a new object (user)
+      const { password, ...user } = insertedUser;
+      // Get the host + port (localhost:3000) from the request headers
+      const host = req.get('host');
+      // Compute the full location, e.g. http://localhost:3000/api/users/132
+      // This will help the client know where the new resource can be found!
+      const location = `http://${host}${req.url}/${user.id}`;
+      return res
+        .status(201)
+        .set('Location', location)
+        .json(user);
+    });
   });
 });
 
