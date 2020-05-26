32a1f2ee694f83aa8d17db787e0794ab76f06363
Catherine <catblecon@gmail.com>
2020-05-26 21:59:34 +0100

    put

--- index.js
+++ index.js
@@ -35,16 +35,19 @@ const userValidationMiddlewares = [
   check('name').isLength({ min: 2 }),
 ];
 
-app.post(
-  '/api/users',
+app.put(
+  '/api/users/:id',
   userValidationMiddlewares,
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
+    // récupération des données envoyées
+    const idUser = req.params.id;
+    const formData = req.body;
     // send an SQL query to get all users
-    return connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
+    return connection.query('UPDATE user SET ? WHERE id = ?', [formData, idUser], (err) => {
       if (err) {
         // If an error has occurred, then the client is informed of the error
         return res.status(500).json({
@@ -52,25 +55,26 @@ app.post(
           sql: err.sql,
         });
       }
-      // We use the insertId attribute of results to build the WHERE clause
-      return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
+      // answer
+      return connection.query('SELECT * FROM user WHERE id = ?', idUser, (err2, records) => {
         if (err2) {
           return res.status(500).json({
             error: err2.message,
             sql: err2.sql,
           });
         }
+
         // If all went well, records is an array, from which we use the 1st item
-        const insertedUser = records[0];
+        const updatedUser = records[0];
         // Extract all the fields *but* password as a new object (user)
-        const { password, ...user } = insertedUser;
+        const { password, ...user } = updatedUser;
         // Get the host + port (localhost:3000) from the request headers
         const host = req.get('host');
         // Compute the full location, e.g. http://localhost:3000/api/users/132
         // This will help the client know where the new resource can be found!
         const location = `http://${host}${req.url}/${user.id}`;
         return res
-          .status(201)
+          .status(200)
           .set('Location', location)
           .json(user);
       });
