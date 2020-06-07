f984aa2bd1846e498b14c35f042b551efe85ccca
K0Si-003 <hugo.pioda@gmail.com>
2020-06-07 16:39:22 +0200

    challenge OK

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// respond to requests on `/api/users`
 app.get('/api/users', (req, res) => {
   // send an SQL query to get all users
   connection.query('SELECT * FROM user', (err, results) => {
@@ -82,7 +81,41 @@ app.put(
   '/api/users/:id',
   userValidationMiddlewares,
   (req, res) => {
-
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    return connection.query('UPDATE user SET ? WHERE id = ?', [req.body, req.params.id], (err, results) => {
+      if (err) {
+        // If an error has occurred, then the client is informed of the error
+        return res.status(500).json({
+          error: err.message,
+          sql: err.sql
+        });
+      }
+      // We use the insertId attribute of results to build the WHERE clause
+      return connection.query('SELECT * FROM user WHERE id = ?', req.params.id, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql
+          });
+        }
+        // If all went well, records is an array, from which we use the 1st item
+        const updatedUser = records[0];
+        // Extract all the fields *but* password as a new object (user)
+        const { password, ...user } = updatedUser;
+        // Get the host + port (localhost:3000) from the request headers
+        const host = req.get('host');
+        // Compute the full location, e.g. http://localhost:3000/api/users/132
+        // This will help the client know where the new resource can be found!
+        const location = `http://${host}${req.url}/${user.id}`;
+        return res
+          .status(200)
+          .set('Location', location)
+          .json(user);
+      });
+    });
   }
 );
 
