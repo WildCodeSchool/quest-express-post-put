8ae45a65dfd00b6fbcab4d78799cef8b6470996f
Floran <70599734+e-floran@users.noreply.github.com>
2020-12-06 19:55:00 +0100

    Update index.js
    Finished quest

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// dotenv loads parameters (port and database config) from .env
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,29 +8,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// respond to requests on `/api/users`
 app.get('/api/users', (req, res) => {
-  // send an SQL query to get all users
   connection.query('SELECT * FROM user', (err, results) => {
     if (err) {
-      // If an error has occurred, then the client is informed of the error
       res.status(500).json({
         error: err.message,
         sql: err.sql,
       });
     } else {
-      // If everything went well, we send the result of the SQL query as JSON
       res.json(results);
     }
   });
 });
 
 const userValidationMiddlewares = [
-  // email must be valid
   check('email').isEmail(),
-  // password must be at least 8 chars long
   check('password').isLength({ min: 8 }),
-  // let's assume a name should be 2 chars long
   check('name').isLength({ min: 2 }),
 ];
 
@@ -43,16 +35,13 @@ app.post(
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    // send an SQL query to get all users
     return connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
       if (err) {
-        // If an error has occurred, then the client is informed of the error
         return res.status(500).json({
           error: err.message,
           sql: err.sql,
         });
       }
-      // We use the insertId attribute of results to build the WHERE clause
       return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
         if (err2) {
           return res.status(500).json({
@@ -60,14 +49,44 @@ app.post(
             sql: err2.sql,
           });
         }
-        // If all went well, records is an array, from which we use the 1st item
         const insertedUser = records[0];
-        // Extract all the fields *but* password as a new object (user)
         const { password, ...user } = insertedUser;
-        // Get the host + port (localhost:3000) from the request headers
         const host = req.get('host');
-        // Compute the full location, e.g. http://localhost:3000/api/users/132
-        // This will help the client know where the new resource can be found!
+        const location = `http://${host}${req.url}/${user.id}`;
+        return res
+          .status(201)
+          .set('Location', location)
+          .json(user);
+      });
+    });
+  },
+);
+
+api.put(
+  '/api/users/:id',
+  userValidationMiddlewares,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    return connection.query('UPDATE users SET ? WHERE id = ?', req.body, (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      }
+      return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });
+        }
+        const insertedUser = records[0];
+        const { password, ...user } = insertedUser;
+        const host = req.get('host');
         const location = `http://${host}${req.url}/${user.id}`;
         return res
           .status(201)
