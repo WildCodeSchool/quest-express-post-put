be01b4be64b81d76e23a8dc1d21158cacbaa0357
thomas <thomas.chalanson@sfr.fr>
2020-12-28 18:57:25 +0100

    do the POST request

--- index.js
+++ index.js
@@ -25,6 +25,62 @@ app.get('/api/users', (req, res) => {
   });
 });
 
+app.get('/api/suppliers', (req, res) => {
+  // send an SQL query to get all users
+  connection.query('SELECT * FROM suppliers', (err, results) => {
+    if (err) {
+      // If an error has occurred, then the client is informed of the error
+      res.status(500).json({
+        error: err.message,
+        sql: err.sql,
+      });
+    } else {
+      // If everything went well, we send the result of the SQL query as JSON
+      res.json(results);
+    }
+  });
+});
+
+/* *************** POST ***************** */
+app.use(express.json());
+
+app.post('/api/users', (req, res) => {
+  // send an SQL query to get all users
+  const { email, password, name } = req.body;
+  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
+  if (!email || !name || !password) {
+    return res.status(422).json({
+      error: 'at least one of the required fields is missing',
+    });
+  }
+  if (password.length < 8) {
+    return res.status(422).json({
+      error: 'Password too short (8 characters min.)',
+    });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(422).json({
+      error: 'Invalid email',
+    });
+  }
+  // send an SQL query to get all users
+  return connection.query(
+    'INSERT INTO user SET ?',
+    req.body,
+    (err, results) => {
+      if (err) {
+        // If an error has occurred, then the client is informed of the error
+        return res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      }
+      // If everything went well, we send the result of the SQL query as JSON
+      return res.json(results);
+    }
+  );
+});
+
 app.listen(process.env.PORT, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
