eceb9030cdc64207faccbf7a1b3e317952758cc9
thomas <thomas.chalanson@sfr.fr>
2020-12-29 14:16:50 +0100

    juste some test

--- index.js
+++ index.js
@@ -38,23 +38,37 @@ app.post(
     }
 
     // send an SQL query to get all users
-    return connection.query(
-      'INSERT INTO user SET ?',
-      req.body,
-      (err, results) => {
-        if (err) {
-          // If an error has occurred, then the client is informed of the error
-          return res.status(500).json({
-            error: err.message,
-            sql: err.sql,
-          });
-        }
-        // If everything went well, we send the result of the SQL query as JSON
-        return res.json(results.insertID);
-      },
-    );
-  },
-);
+    return connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
+      if (err) {
+        // If an error has occurred, then the client is informed of the error
+        return res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      }
+
+ // We use the insertId attribute of results to build the WHERE clause
+ return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
+  if (err2) {
+    return res.status(500).json({
+      error: err2.message,
+      sql: err2.sql,
+    });
+  }
+  // If all went well, records is an array, from which we use the 1st item
+  const insertedUser = records[0];
+  // Extract all the fields *but* password as a new object (user)
+  const { password, ...user } = insertedUser;
+  // Get the host + port (localhost:3000) from the request headers
+  const host = req.get('host');
+  // Compute the full location, e.g. http://localhost:3000/api/users/132
+  // This will help the client know where the new resource can be found!
+  const location = `http://${host}${req.url}/${user.id}`;
+  return res
+    .status(201)
+    .set('Location', location)
+    .json(user);
+});
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
