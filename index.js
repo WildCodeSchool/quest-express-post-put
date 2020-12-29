76330c463839110569a19d13762ab34d34b787d9
thomas <thomas.chalanson@sfr.fr>
2020-12-29 12:34:20 +0100

    express validator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
 const connection = require('./db');
 
 const app = express();
@@ -24,6 +25,28 @@ app.get('/api/users', (req, res) => {
     }
   });
 });
+
+const User = '';
+app.post(
+  '/api/users',
+  // username must be an email
+  body('username').isEmail(),
+  // password must be at least 5 chars long
+  body('password').isLength({ min: 5 }),
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      username: req.body.username,
+      password: req.body.password,
+    }).then((user) => res.json(user));
+  },
+);
+
 app.listen(process.env.PORT, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
