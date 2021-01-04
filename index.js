aee8fb552cc66f58e7fb2dd6c283accf55c5c6be
benoitfinet <benoitfinetpro@gmail.com>
2021-01-04 15:34:31 +0100

    challenge express 7

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 // dotenv loads parameters (port and database config) from .env
 require('dotenv').config();
 const express = require('express');
@@ -68,6 +69,41 @@ app.post(
         const host = req.get('host');
         // Compute the full location, e.g. http://localhost:3000/api/users/132
         // This will help the client know where the new resource can be found!
+        const location = `http://${host}${req.url}/${user.id}`;
+        return res
+          .status(201)
+          .set('Location', location)
+          .json(user);
+      });
+    });
+  },
+);
+
+app.put(
+  '/api/users/:id',
+  userValidationMiddlewares,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    return connection.query('UPDATE INTO user SET ?', req.body, (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      }
+      return connection.query('SELECT * FROM user WHERE id = ?', results.req.params.id, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });
+        }
+        const insertedUser = records[0];
+        const { password, ...user } = insertedUser;
+        const host = req.get('host');
         const location = `http://${host}${req.url}/${user.id}`;
         return res
           .status(201)
