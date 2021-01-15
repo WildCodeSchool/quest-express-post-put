76206cc51ac255165e0560df92e6b6e2961a84e9
Gabriellji <gabriellji@yahoo.com>
2021-01-15 20:27:13 +0300

    feat: modify post mth

--- index.js
+++ index.js
@@ -27,11 +27,8 @@ app.get('/api/users', (req, res) => {
 });
 
 app.post('/api/users', [
-  // email must be valid
   check('email').isEmail(),
-  // password must be at least 8 chars long
   check('password').isLength({ min: 8 }),
-  // let's assume a name should be 2 chars long
   check('name').isLength({ min: 2 }),
 ],
 (req, res) => {
@@ -39,17 +36,39 @@ app.post('/api/users', [
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
-  // send an SQL query to get all users
   return connection.query('INSERT INTO user SET ?', req.body, (err, results) => {
     if (err) {
-      // If an error has occurred, then the client is informed of the error
+      if (err.code === 'ER_DUP_ENTRY') {
+        return res.status(409).json({
+          error: 'Email already exists',
+        });
+      }
       return res.status(500).json({
         error: err.message,
         sql: err.sql,
       });
     }
-    // If everything went well, we send the result of the SQL query as JSON
-    return res.json(results);
+    return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
+      if (err2) {
+        return res.status(500).json({
+          error: err2.message,
+          sql: err2.sql,
+        });
+      }
+      // If all went well, records is an array, from which we use the 1st item
+      const insertedUser = records[0];
+      // Extract all the fields *but* password as a new object (user)
+      const { password, ...user } = insertedUser;
+      // Get the host + port (localhost:3000) from the request headers
+      const host = req.get('host');
+      // Compute the full location, e.g. http://localhost:3000/api/users/132
+      // This will help the client know where the new resource can be found!
+      const location = `http://${host}${req.url}/${user.id}`;
+      return res
+        .status(201)
+        .set('Location', location)
+        .json(user);
+    });
   });
 });
 
