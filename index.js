4ae0e4547a8ce6f1257e3737c380f587af012d07
INGRID <ingridpruvost966@gmail.com>
2021-03-18 20:05:45 +0100

    challenge ok

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // dotenv loads parameters (port and database config) from .env
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator');
 const connection = require('./db');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // respond to requests on `/api/users`
 app.get('/api/users', (req, res) => {
@@ -24,6 +23,21 @@ app.get('/api/users', (req, res) => {
       res.json(results);
     }
   });
+});
+
+app.get("/api/users/:id", (req, res) => {
+  connection.query(
+    "SELECT * from user WHERE id=?",
+    [req.params.id],
+    (err, results) => {
+      if (err) {  
+        console.log(err);
+        res.status(500).send("Error retrieving data");
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  );
 });
 
 const userValidationMiddlewares = [
@@ -71,6 +85,47 @@ app.post(
         const location = `http://${host}${req.url}/${user.id}`;
         return res
           .status(201)
+          .set('Location', location)
+          .json(user);
+      });
+    });
+  },
+);
+
+app.put(
+  '/api/users/:id',
+  userValidationMiddlewares,
+  
+  (req, res) => {
+    const idUser = req.params.id
+    const newUser = req.body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    return connection.query(
+      'UPDATE user SET ? WHERE id = ? ',
+    [newUser, idUser],
+     (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      }
+      return connection.query('SELECT * FROM user WHERE id = ?', req.params.id, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });
+        }
+        const insertedUser = records[0];
+        const { password, ...user } = insertedUser;
+        const host = req.get('host');
+        const location = `http://${host}${req.url}/${user.id}`;
+        return res
+          .status(200)
           .set('Location', location)
           .json(user);
       });
